2482f173844202dc98af799bff6dedc3e587a485
MichaelWMoore <mwmoore808@hotmail.com>
2019-12-26 13:15:58 -0700

    Initial Commit

---
+++ CyclicalAssets/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CyclicalAssets
+//
+//  Created by Michael W. Moore on 12/26/19.
+//  Copyright © 2019 BigNerdRanch. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
